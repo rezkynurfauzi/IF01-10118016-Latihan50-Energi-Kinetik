1f35f64e733902918ba41179be9e88c433ba44d8
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:42:44 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan50-Energi Kinetik/src/if01/pkg10118016/latihan50/energi/kinetik/IF0110118016Latihan50EnergiKinetik.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan50.energi.kinetik;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Energi Kinetik
+ */
+
+public class IF0110118016Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Energi kinetik = new Energi();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("=====PROGRAM MENCARI ENERGI KINETIK=====");
+        System.out.println("Energi Kinetik : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
